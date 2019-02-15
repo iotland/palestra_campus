ee5f647e8041a724e5d57c25eccd5722de3e560d
iotland <47613086+iotland@users.noreply.github.com>
2019-02-15 09:34:52 -0200

    Update palestra_campus1.ino.ino

--- palestra_campus1.ino/palestra_campus1.ino.ino
+++ palestra_campus1.ino/palestra_campus1.ino.ino
@@ -1,11 +1,50 @@
-int in1 = 7;
-void setup() {
-  pinMode(in1, OUTPUT);
-  digitalWrite(in1, HIGH);
+#define pino_sinal_analogico A0 //define o pino que o sensor de solo vai estar alocado
+int rele = 7; //define o pino que o modulo rele vai estar alocado 
+ 
+int valor_analogico; 
+ 
+void setup()
+{
+  Serial.begin(9600);//inicia o serial print do arduino 
+  pinMode(pino_sinal_analogico, INPUT); //inicia o sensor de umidade 
+  pinMode(rele, OUTPUT); 
+  digitalWrite(rele, HIGH);
 }
-void loop() {
-  digitalWrite(in1, LOW);
-  delay(100);
-  digitalWrite(in1, HIGH);
+ 
+void loop()
+{
+  //Le o valor do pino A0 do sensor
+  valor_analogico = analogRead(pino_sinal_analogico);
+ 
+  //Mostra o valor da porta analogica no serial monitor
+  Serial.print("Porta analogica: ");
+  Serial.print(valor_analogico);
+ 
+  //Solo umido
+  if (valor_analogico > 0 && valor_analogico < 400)
+  {
+    Serial.println(" Status: Solo umido"); //mostra no serial print o estado do solo
+  
+  }
+ 
+  //Solo com umidade moderada
+  if (valor_analogico > 400 && valor_analogico < 800)
+  {
+    Serial.println(" Status: Umidade moderada"); //mostra no serial print o estado do solo
+    digitalWrite(rele, LOW); 
+    delay(3000);
+    digitalWrite(rele, HIGH);
+    delay(3000);
+  }
+ 
+  //Solo seco
+  if (valor_analogico > 800 && valor_analogico < 1024)
+  {
+    Serial.println(" Status: Solo seco"); //mostra no serial print o estado do solo
+    digitalWrite(rele, LOW);
+    delay(6000);
+    digitalWrite(rele, HIGH);
+    delay(6000);
+  }
   delay(100);
 }
